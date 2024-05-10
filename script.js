4ea4555ec536ad29efd8b2c9c33cb9d1db80a69b
Aravind Sasidharan <158824483+Aravindvidya@users.noreply.github.com>
2024-05-10 10:11:18 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+
+
+const searchBarContainer= document.querySelector(".searchContainer");
+const maginifier= document.querySelector(".magnifier")
+searchBarContainer.addEventListener('click',()=>{
+   searchBarContainer.classList.toggle('active');
+})
